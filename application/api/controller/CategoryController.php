d5324fae72d374a514b3ddbc576b35b3c4e1e08d
onep2p <324834500@qq.com>
2018-06-05 15:02:51 +0800

    新增分类接口

--- application/api/controller/CategoryController.php
+++ application/api/controller/CategoryController.php
@@ -6,7 +6,21 @@ use think\Request;
 
 class CategoryController extends Controller{
 
+    /**
+    * 返回商品分类
+    * @date: 2018年6月5日 下午2:59:10
+    * @author: onep2p <324834500@qq.com>
+    * @param: variable
+    * @return:
+    */
     public function  category(Request $request){
-
+        $model = db('product_category');
+        $list = $model->where('status > 0')->order('sort asc')->select();
+        
+        if($list){
+            return json(['code'=>0, 'msg'=>'调用成功', 'data'=>$list]);
+        }else{
+            return json(['code'=>1, 'msg'=>'调用失败', 'data'=>[]]);
+        }
     }
 }
