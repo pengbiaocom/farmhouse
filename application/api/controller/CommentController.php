901e55e925591186b2b554eca650b111797340bc
pengqq <pengbiaocom56@163.com>
2018-06-12 16:54:30 +0800

    上传评价api控制器

---
+++ application/api/controller/CommentController.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\api\controller;
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/6/12
+ * Time: 16:53
+ */
+use think\Controller;
+use think\Request;
+
+class CommentController extends Controller{
+
+    public function index(Request $request){
+
+    }
+}
