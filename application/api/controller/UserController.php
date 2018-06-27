fcedb56ff0357ca2a7fe1a8602fed0fa47fd341b
onep2p <324834500@qq.com>
2018-06-27 10:46:54 +0800

    update

--- application/api/controller/UserController.php
+++ application/api/controller/UserController.php
@@ -12,6 +12,16 @@ class UserController extends  Controller{
     private $sessionKey = '';
     
     public function login(Request $request){
+        $ucenterMemberModel = new UcenterMemberModel();
+        $openid = $request->param('openid');
+        $user = $ucenterMemberModel::get(function($query) use($openid){
+            $query->where("openid", $openid);
+        });
+        echo $ucenterMemberModel->getLastSql();
+        var_dump($user);
+        exit;
+        
+        
         $code = $request->param('code', '', 'op_t');
         $encryptedData = $request->param('encryptedData', '', 'op_t');
         $iv = $request->param('iv', '', 'op_t');
@@ -32,7 +42,7 @@ class UserController extends  Controller{
         
         if($res && !empty($res['openid'])){
             $ucenterMemberModel = new UcenterMemberModel();
-            $user = $ucenterMemberModel::get(function($query){
+            $user = $ucenterMemberModel::get(function($query) use($res){
                 $query->where('openid', $res['openid']);
             });
             
